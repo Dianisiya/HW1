f9440cc0c4cf9a6cc4a94e8b177c800efda12770
Dianisiya <dianisiya.kolisnichenko@gmail.com>
2017-07-18 18:04:41 +0300

    Add missed files

--- Wheather/Wheather/Global.asax.cs
+++ Wheather/Wheather/Global.asax.cs
@@ -11,6 +11,8 @@ using Wheather.Services.Interfaces;
 
 namespace Wheather
 {
+    using System.Data.Entity;
+
     using Wheather.Models.Db;
 
     public class MvcApplication : System.Web.HttpApplication
@@ -27,6 +29,9 @@ namespace Wheather
             container.Register<IRequestService, RequestService>(new PerScopeLifetime());
             container.Register<IActionLogger, DbActionLogger>(new PerScopeLifetime());
             container.Register<WeatherDb>(new PerRequestLifeTime());
+            container.Register<IRepository<Action, int>, Repository<Action, int>>(new PerScopeLifetime());
+            container.Register<IRepository<City, int>, Repository<City, int>>(new PerScopeLifetime());
+            container.Register<IRepository<Weather, int>, Repository<Weather, int>>(new PerScopeLifetime());
 
             container.EnableMvc();
         }
